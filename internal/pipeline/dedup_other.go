42bea3b10264aeaaa3e74426c5daf6e992e8371d
Mero <mero@localhost>
2026-05-27 00:53:02 +0300

    fix: define mmapMaxFileSize on non-unix platforms so tests compile on Windows

--- internal/pipeline/dedup_other.go
+++ internal/pipeline/dedup_other.go
@@ -7,6 +7,8 @@ import (
 	"os"
 )
 
+const mmapMaxFileSize = int64(1<<32 - 1) // mirrors dedup_unix.go; mmap never used on this platform
+
 func canMmapDedup(_ int64) bool { return false }
 
 func mmapDedup(_ *Model, _ *os.File, _ int64, _ *bufio.Writer) error {
